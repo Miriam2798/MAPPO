19900e553b5445b055d7c5513d10650c44087b98
Miriam_Elbaz <miriam.elbaz@estudiantat.upc.edu>
2022-03-21 19:27:06 +0100

    lunes 21/03

--- comp_poll.py
+++ comp_poll.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
-df = pd.read_excel(r'C:\Users\miria\OneDrive\Desktop\Uni\4A\PAE\1.A.3.b.i-iv Road Transport Appendix 4 Emission Factors 2021.xlsx',hoja = 'HOT_EMISSIONS_PARAMETERS')
+df = pd.read_excel(r'\\wsl.localhost\Ubuntu-20.04\home\miriam27\PAE\MAPPO\1.A.3.b.i-iv Road Transport Appendix 4 Emission Factors 2021.xlsx',hoja = 'HOT_EMISSIONS_PARAMETERS')
 
+#SO2
+#ESO2m = 2 * Ksm * FCm
+
+df.head()
