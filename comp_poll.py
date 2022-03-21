fafab123c107a78b75e3d727a72133233aaf1fb7
Miriam_Elbaz <miriam.elbaz@estudiantat.upc.edu>
2022-03-21 18:36:50 +0100

    primer commit

---
+++ comp_poll.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_excel(r'C:\Users\miria\OneDrive\Desktop\Uni\4A\PAE\1.A.3.b.i-iv Road Transport Appendix 4 Emission Factors 2021.xlsx',hoja = 'HOT_EMISSIONS_PARAMETERS')
+
