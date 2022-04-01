6936ac656757d674210cf6281ee12d18a20d3018
Xavier <xponsmarti@gmail.com>
2022-04-01 23:10:35 +0200

    system integration new version

--- System-Integration/mappo-server/webapp/app.py
+++ System-Integration/mappo-server/webapp/app.py
@@ -1,13 +1,28 @@
 # Import OS to get the port environment variable from the Procfile
 import os # <-----
+import os.path
+import taxicab as tc
+import osmnx as ox
+import networkx as nx
+import time
+import json
+import requests
 # Import the flask module
 from flask import Flask
 
 # Create a Flask constructor. It takes name of the current module as the argument
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    statement = 'Hello World!'
-    return statement
+def importFile():
+    place = "Barcelona"
+    if os.path.exists(f'{place}_graph.txt'):
+        o = ox.load_graphml(f'{place}_graph.txt')
+        return str(o)
+    #print("\nFirst time running the script for " + place +
+        #  ". Loading and Saving graph...\n")
+    G = ox.graph_from_place(place, network_type='drive')
+    ox.save_graphml(G, f"{place}_graph.txt")
+    return str(G)
 
--- System-Integration/mappo-server/webapp/main.py
+++ System-Integration/mappo-server/webapp/main.py
@@ -1,2 +1,15 @@
+from importlib.resources import path
+import requests
 from app import app
+
+
+
+r= requests.get('https://mappo-server.herokuapp.com/')
+
+with open(r'/usr/src/app/mappo-server/webapp/prova.txt', 'wb') as f:
+    f.write(r.content)
+place = "Barcelona"
+
 app.run()
+
+
