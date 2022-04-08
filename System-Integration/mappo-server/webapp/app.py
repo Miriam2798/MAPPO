326c7bcd2c91d0d932872f895877470aae3a2a83
Xavier <xponsmarti@gmail.com>
2022-04-08 21:31:38 +0200

    ultim push

--- System-Integration/mappo-server/webapp/app.py
+++ System-Integration/mappo-server/webapp/app.py
@@ -1,28 +1,35 @@
 # Import OS to get the port environment variable from the Procfile
 import os # <-----
-import os.path
-import taxicab as tc
-import osmnx as ox
-import networkx as nx
-import time
 import json
 import requests
+import osmnx as ox
+import networkx as nx
+import taxicab as tc
+import time
+import os.path
+import numpy as np
 # Import the flask module
-from flask import Flask
+from flask import Flask, request
+import fastestpath as fp
+import ranking as rk
 
 # Create a Flask constructor. It takes name of the current module as the argument
 app = Flask(__name__)
 
+@app.route('/fastestroute', methods = ['GET'])
 
-@app.route('/')
-def importFile():
-    place = "Barcelona"
-    if os.path.exists(f'{place}_graph.txt'):
-        o = ox.load_graphml(f'{place}_graph.txt')
-        return str(o)
-    #print("\nFirst time running the script for " + place +
-        #  ". Loading and Saving graph...\n")
-    G = ox.graph_from_place(place, network_type='drive')
-    ox.save_graphml(G, f"{place}_graph.txt")
-    return str(G)
+def server_fastest_route():
+    place = "Terrassa"
+    originx = request.args.get("originx")
+    originy = request.args.get("originy")
+    destinationx = request.args.get("destinationx")
+    destinationy = request.args.get("destinationy")
+
+    ruta = fp.server(originx, originy, destinationx, destinationy, place)
+    return ruta
+    
+@app.route('/rank', methods = ['GET'])
+
+def server_rank():
+    return str(rk.rank())
 
--- System-Integration/mappo-server/webapp/main.py
+++ System-Integration/mappo-server/webapp/main.py
@@ -1,15 +1,16 @@
 from importlib.resources import path
 import requests
 from app import app
+import fastestpath as fp
 
 
+#r= requests.get('http://mappo-server.herokuapp.com/')
 
-r= requests.get('https://mappo-server.herokuapp.com/')
-
-with open(r'/usr/src/app/mappo-server/webapp/prova.txt', 'wb') as f:
-    f.write(r.content)
-place = "Barcelona"
+#with open(r'/usr/src/app/mappo-server/webapp/prova.txt', 'wb') as f:
+    #f.write(r.content)
+#place = "Barcelona"
 
 app.run()
+
 
 
